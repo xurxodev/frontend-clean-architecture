d42af0cf58b5b2f8fde3f988dce1ff77056bac63
Jorge Sánchez <xurxosf@gmail.com>
2020-12-07 09:08:00 +0100

    Add .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+/** @format */
+
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "es5",
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    arrowParens: "avoid",
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    proseWrap: "preserve",
+    requirePragma: false,
+    insertPragma: false,
+};
