2971c13260be21d11e65a64ee5592ab493145a44
Jorge Sánchez <xurxosf@gmail.com>
2021-05-07 14:07:07 +0200

    Add react logo to react app bar

--- packages/react-app/src/appbar/MyAppBar.tsx
+++ packages/react-app/src/appbar/MyAppBar.tsx
@@ -4,7 +4,8 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Badge from "@material-ui/core/Badge";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import logo from "./Logo.png";
+import logo from "./logo.png";
+import reactLogo from "./react-logo.png";
 import { useCartBloc } from "../app/App";
 import { UpdatedCartState } from "@frontend-clean-architecture/core";
 import { usePlocState } from "../common/usePlocState";
@@ -21,13 +22,16 @@ const MyAppBar: React.FC = () => {
     const ploc = useCartBloc();
     const state = usePlocState(ploc);
 
-    const totalItems =
-        state.kind === "UpdatedCartState" ? (state as UpdatedCartState).totalItems : 0;
+    const totalItems = state.kind === "UpdatedCartState" ? state.totalItems : 0;
 
     return (
         <AppBar position="static">
             <Toolbar className={classes.toolbar}>
-                <img src={logo} width={150} alt="logo" />
+                <div>
+                    <img src={logo} width={150} alt="logo" />
+                    <img src={reactLogo} width={40} alt="react logo" />
+                </div>
+
                 <IconButton color="inherit">
                     <Badge badgeContent={totalItems} color="secondary">
                         <ShoppingCartIcon onClick={() => ploc.openCart()} />
