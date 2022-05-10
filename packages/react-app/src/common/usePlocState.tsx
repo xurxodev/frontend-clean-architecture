4fbe65584195b593e4feecc26f8904992923ce45
mahdi khanbeigi <mahdi.khanbeigi@yahoo.com>
2022-05-10 11:30:15 +0430

    Return to the previous code

--- packages/react-app/src/common/usePlocState.tsx
+++ packages/react-app/src/common/usePlocState.tsx
@@ -1,19 +1,13 @@
 import { Ploc } from "@frontend-clean-architecture/core";
 import { useEffect, useState } from "react";
-
 export function usePlocState<S>(ploc: Ploc<S>) {
     const [state, setState] = useState(ploc.state);
-
     useEffect(() => {
         const stateSubscription = (state: S) => {
             setState(state);
-            ploc.unsubscribe(stateSubscription)
         };
-
         ploc.subscribe(stateSubscription);
-
         return () => ploc.unsubscribe(stateSubscription);
-    }, []);
-
+    }, [ploc]);
     return state;
 }
