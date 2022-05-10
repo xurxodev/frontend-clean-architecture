097ff669ca30cf721cbba83e2d586a7dbe9c117d
mahdi khanbeigi <mahdi.khanbeigi@yahoo.com>
2022-05-10 11:29:38 +0430

    change call ploc outside rendered

--- packages/react-app/src/products/ProductList.tsx
+++ packages/react-app/src/products/ProductList.tsx
@@ -1,17 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { CircularProgress, Grid, Container, Box, Typography } from "@material-ui/core";
 import ProductItem from "./ProductItem";
 import { dependenciesLocator } from "@frontend-clean-architecture/core";
 import { usePlocState } from "../common/usePlocState";
+
+const ploc = dependenciesLocator.provideProductsPloc();
 const useStyles = makeStyles(theme => ({
-    titleContainer: {
-        marginBottom: theme.spacing(4),
-    },
-    cardGrid: {
-        paddingTop: theme.spacing(4),
-        paddingBottom: theme.spacing(4),
-    },
+    titleContainer: { marginBottom: theme.spacing(4) },
+    cardGrid: { paddingTop: theme.spacing(4), paddingBottom: theme.spacing(4) },
     infoContainer: {
         display: "flex",
         alignItems: "center",
@@ -19,17 +16,13 @@ const useStyles = makeStyles(theme => ({
         height: "100vh",
     },
 }));
-
 const ProductList: React.FC = () => {
-    const ploc = dependenciesLocator.provideProductsPloc();
     const classes = useStyles();
     const state = usePlocState(ploc);
-
     React.useEffect(() => {
         const searchProducts = async (filter: string) => {
             ploc.search(filter);
         };
-
         searchProducts("Element");
     }, [ploc]);
 
@@ -37,39 +30,46 @@ const ProductList: React.FC = () => {
         case "LoadingProductsState": {
             return (
                 <div className={classes.infoContainer}>
-                    <CircularProgress />
+                    {" "}
+                    <CircularProgress />{" "}
                 </div>
             );
         }
         case "ErrorProductsState": {
             return (
                 <div className={classes.infoContainer}>
+                    {" "}
                     <Typography display="inline" variant="h5" component="h2">
-                        {state.error}
-                    </Typography>
+                        {" "}
+                        {state.error}{" "}
+                    </Typography>{" "}
                 </div>
             );
         }
         case "LoadedProductsState": {
             return (
                 <Container className={classes.cardGrid} maxWidth="xl">
+                    {" "}
                     <Box className={classes.titleContainer}>
+                        {" "}
                         <Typography display="inline" variant="h6" component="h2">
-                            {"Results for "}
-                        </Typography>
+                            {" "}
+                            {"Results for "}{" "}
+                        </Typography>{" "}
                         <Typography color="primary" display="inline" variant="h6" component="h2">
-                            {"Element"}
-                        </Typography>
-                    </Box>
+                            {" "}
+                            {"Element"}{" "}
+                        </Typography>{" "}
+                    </Box>{" "}
                     <Grid container spacing={2}>
+                        {" "}
                         {state.products.map((product, index) => (
                             <ProductItem product={product} key={index} />
-                        ))}
-                    </Grid>
+                        ))}{" "}
+                    </Grid>{" "}
                 </Container>
             );
         }
     }
 };
-
 export default ProductList;
